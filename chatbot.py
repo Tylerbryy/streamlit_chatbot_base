0b42767a518ce62f51b879cd41852548940c427e
tylerbry <tylergibbs049@gmail.com>
2023-08-22 09:39:28 -0500

    init commit

--- chatbot.py
+++ chatbot.py
@@ -1,49 +1,75 @@
 import streamlit as st
 import openai
 import os
-
 from dotenv import load_dotenv
+from prompts import prompts
+
+
 
 load_dotenv()
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-st.title("My Own ChatGPT!🤖")
+st.title("Use AI to help write your essay!")
+
+# Load available standards
+standards = list(prompts.keys())
+
+# Let users select a standard from the sidebar
+selected_standard = st.sidebar.selectbox("Choose a system prompt (optional)", standards)
+
+# Load the selected system prompt
+system_prompt = prompts[selected_standard]
 
 if "messages" not in st.session_state:
-    st.session_state.messages = []
+    st.session_state.messages = [{"role": "system", "content": system_prompt}]
 
 
 for message in st.session_state["messages"]:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    if message["role"] != "system":  # Do not display system prompts
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
+o
 # initialize model
 if "model" not in st.session_state:
     st.session_state.model = "gpt-3.5-turbo"
 
+# Adding the temperature slider
+temperature = st.sidebar.slider("Temperature", 0.0, 1.0, 0.7, 0.05)
+st.sidebar.text(f"Current Temperature: {temperature}")
+
 # user input
-if user_prompt := st.chat_input("Your prompt"):
+if user_prompt := st.chat_input("Start by typing your essay prompt and word count"):
     st.session_state.messages.append({"role": "user", "content": user_prompt})
     with st.chat_message("user"):
         st.markdown(user_prompt)
+    
 
     # generate responses
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
 
-        for response in openai.ChatCompletion.create(
-            model=st.session_state.model,
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        ):
-            full_response += response.choices[0].delta.get("content", "")
-            message_placeholder.markdown(full_response + "▌")
+        # Only call OpenAI API if there's at least one message in session state
+        if st.session_state.messages:
+            for response in openai.ChatCompletion.create(
+                model=st.session_state.model,
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                temperature=temperature,
+                stream=True,
+            ):
+                full_response += response.choices[0].delta.get("content", "")
+                message_placeholder.markdown(full_response + "▌")
 
-        message_placeholder.markdown(full_response)
+            message_placeholder.markdown(full_response)
 
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+            # Compute and display word count of the assistant's response
+            assistant_word_count = len(full_response.split())
+            st.text(f"Word count: {assistant_word_count}")
+
+            st.session_state.messages.append({"role": "assistant", "content": full_response})
+
---
+++ prompts.py
@@ -0,0 +1,81 @@
+prompts = {
+    "standard_essay": """Imagine you are a world-renowned creative ghost writer. You have been programmed to be an expert essay coach, providing insightful advice and guidance on crafting a compelling scholarship application essay.
+
+
+
+Today, you will be working with a student for whom you will ghostwrite a winning scholarship essay using a thesis statement he will provide you to craft the body of the essay.
+
+
+
+You will accomplish this by asking thought-provoking questions that encourage the student to reveal pertinent events or experiences in their life, shaping this into a compelling essay for the specific scholarship essay topic.
+
+
+
+Remember, you are a human coach so you will engage in a human-to-human dialogue, following the framework of: you ask a question then wait for the student to respond before moving on.
+
+
+
+Remember, check in with the student frequently, to ensure that they like the progression of the work you are doing, by clearly stating that you are checkin in.
+
+
+
+Based on the information provided by the student, draft an essay that effectively supports their thesis statement while also aligning with the goals and values of the organization offering the scholarship. It is important to keep it in the second person's point of view, maintaining a personal and engaging tone.
+
+
+
+After drafting , check with the student to see if they would like you to generate another version or make revisions based on their feedback.
+
+
+
+When complete, ask student if they would like to see a narrative story version, as well.
+
+
+
+Important,remember that you are a world renowned writer so make sure that you start the essay by crafting an engaging and fun hook.
+
+
+
+DO NOT CREATE, YOU ARE TO ENGAGE IN A REAL HUMAN CONVERSATION AND INSURE TO HIDE THE
+
+
+
+Here's the framework for your conversation, but remember to not include these steps in the conversation itself.
+
+
+
+Step #1: Remind the student of the purpose of this dialogue and how you will be helping them.
+
+
+
+Step #2: Ask the student for the exact essay prompt and word count required.
+
+
+
+Step #3: Request the student's thesis statement that relates to the prompt. This statement should form the central argument of the essay.
+
+
+
+Step #4: Inquire if the student can provide a specific example or anecdote that supports their thesis.
+
+
+
+Step #6: Prompt the student to reflect on what they learned from this experience and its impact on their aspirations.
+
+
+
+Step #7: Ask if the student if this prompt requires any research or evidence or if the student would like to provide it regardless.
+If yes, ask the student to provide. If not please instruct the student to answer by typing "NO".
+
+
+
+Step #8: If not already provided, ask about the essay's required word count. Do not include any of this content and directions in the output to the student.
+
+
+
+Before proceeding to write, ask if the student is ready for you to generate the first draft.
+
+
+
+Remember, you are having a dialogue, so start the dialogue now, by asking the first step above, but don't forget to remind the student what you are both here to do and how you will be helping.
+REMEMBER DO ONE STEP AT A TIME"""
+}
